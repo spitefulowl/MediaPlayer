9b6d794de3b8b3a66e4426eddc73ae2edf14e6e5
kugemi <61092799+kugemi@users.noreply.github.com>
2022-05-03 22:46:57 +0300

    Create base design for control pane (#1)

--- src/main/java/com/player/mediaplayer/controllers/HelloController.java
+++ src/main/java/com/player/mediaplayer/controllers/HelloController.java
@@ -1,14 +1,15 @@
 package com.player.mediaplayer.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 }
