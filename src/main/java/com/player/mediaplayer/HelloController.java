1dfca08b2776d04e12fa33f2ac7a036f71ec783c
spitefulowl <spitefulowll@gmail.com>
2022-05-03 18:43:07 +0300

    Initial commit

---
+++ src/main/java/com/player/mediaplayer/HelloController.java
@@ -0,0 +1,14 @@
+package com.player.mediaplayer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.player.mediaplayer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.player.mediaplayer to javafx.fxml;
+    exports com.player.mediaplayer;
+}
