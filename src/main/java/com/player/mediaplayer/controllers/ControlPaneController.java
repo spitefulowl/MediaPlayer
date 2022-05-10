d4a06e667271e3bbdf9572cd55b436f56de570bb
spitefulowl <54745281+spitefulowl@users.noreply.github.com>
2022-05-10 18:36:19 +0300

    Update some design stuff (#7)
    * Update buttons design, use gradient background
    
    * Implement changing of slider progress color
    
    * Update icons design
    
    * Fix volume slider layout

--- src/main/java/com/player/mediaplayer/controllers/ControlPaneController.java
+++ src/main/java/com/player/mediaplayer/controllers/ControlPaneController.java
@@ -20,9 +20,11 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.File;
 import java.io.IOException;
@@ -73,6 +75,7 @@ public class ControlPaneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setDefaultImages();
+        initializeButtonsIcons();
         playButtonAction();
         openFolderButtonAction();
         shuffleButtonAction();
@@ -81,17 +84,25 @@ public class ControlPaneController implements Initializable {
         initializeDurationSlider();
         initializeOnEndOfMediaAction();
         currentTrackChangedHandler();
-        updateControlsVisibility(true);
+        updateControlsDisable(true);
     }
 
-    private void updateControlsVisibility(Boolean invisible) {
-        playSongButton.setDisable(invisible);
-        previousSongButton.setDisable(invisible);
-        nextSongButton.setDisable(invisible);
-        shuffleButton.setDisable(invisible);
-        repeatSongButton.setDisable(invisible);
-        durationSlider.setDisable(invisible);
-        volumeSlider.setDisable(invisible);
+    private void initializeButtonsIcons() {
+        shuffleButton.setGraphic(new FontIcon());
+        previousSongButton.setGraphic(new FontIcon());
+        nextSongButton.setGraphic(new FontIcon());
+        repeatSongButton.setGraphic(new FontIcon());
+        folderButton.setGraphic(new FontIcon());
+    }
+
+    private void updateControlsDisable(Boolean disabled) {
+        playSongButton.setDisable(disabled);
+        previousSongButton.setDisable(disabled);
+        nextSongButton.setDisable(disabled);
+        shuffleButton.setDisable(disabled);
+        repeatSongButton.setDisable(disabled);
+        durationSlider.setDisable(disabled);
+        volumeSlider.setDisable(disabled);
     }
 
     private void initializeOnEndOfMediaAction() {
@@ -218,7 +229,7 @@ public class ControlPaneController implements Initializable {
     }
 
     private void startTimer() {
-        updateControlsVisibility(false);
+        updateControlsDisable(false);
         if (PlayerContext.globalTimer != null) {
             throw new IllegalStateException("Timer not stopped");
         }
@@ -229,7 +240,11 @@ public class ControlPaneController implements Initializable {
                 double currentTime = player.getMediaPlayer().getCurrentTime().toSeconds();
                 double totalDuration = player.getMediaPlayer().getTotalDuration().toSeconds();
                 if (!durationSlider.isValueChanging()) {
-                    durationSlider.setValue(currentTime / totalDuration);
+                    double newSliderValue = currentTime / totalDuration;
+                    if (Double.isNaN(newSliderValue)) {
+                        newSliderValue = 0;
+                    }
+                    durationSlider.setValue(newSliderValue);
                 }
             }
         };
@@ -248,7 +263,11 @@ public class ControlPaneController implements Initializable {
         volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                player.setCurrentVolume(volumeSlider.getValue());
+                double newVolume = observableValue.getValue().doubleValue();
+                player.setCurrentVolume(newVolume);
+                String style = String.format("-fx-background-color: linear-gradient(to right, #3a3937 %d%%, #a9a9a9 %d%%) !important;",
+                        (int) (newVolume * 100), (int) (newVolume * 100));
+                volumeSlider.lookup(".track").setStyle(style);
             }
         });
         player.getCurrentVolume().addListener(new InvalidationListener() {
@@ -263,15 +282,17 @@ public class ControlPaneController implements Initializable {
         durationSlider.setMin(0);
         durationSlider.setMax(1);
         durationSlider.setValue(0);
-        durationSlider.valueProperty().addListener(new ChangeListener<Number>() {
+        durationSlider.valueProperty().addListener(new ChangeListener<>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                currentDuration.setText(MP3Parser.parseSongLength((int) player.getMediaPlayer().getTotalDuration().multiply(durationSlider.getValue()).toSeconds()));
+                double newCurrentDuration = observableValue.getValue().doubleValue();
+                currentDuration.setText(MP3Parser.parseSongLength((int) player.getMediaPlayer().getTotalDuration().multiply(newCurrentDuration).toSeconds()));
+                String style = String.format("-fx-background-color: linear-gradient(to right, #3a3937 %d%%, #a9a9a9 %d%%) !important;",
+                        (int) (newCurrentDuration * 100), (int) (newCurrentDuration * 100));
+                durationSlider.lookup(".track").setStyle(style);
             }
         });
-        durationSlider.setOnMouseReleased((MouseEvent event) -> {
-            player.getMediaPlayer().seek(player.getMediaPlayer().getTotalDuration().multiply(durationSlider.getValue()));
-        });
+        durationSlider.setOnMouseReleased((MouseEvent event) -> player.getMediaPlayer().seek(player.getMediaPlayer().getTotalDuration().multiply(durationSlider.getValue())));
     }
 
     public void previousButtonAction(ActionEvent actionEvent) {
