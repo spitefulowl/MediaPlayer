6bf1776e474b7164ffa251787fa58c62c16bdde1
spitefulowl <spitefulowll@gmail.com>
2022-05-22 00:12:04 +0300

    Save info about liked tracks

--- src/main/java/com/player/mediaplayer/models/PlayerState.java
+++ src/main/java/com/player/mediaplayer/models/PlayerState.java
@@ -9,37 +9,52 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class PlayerState implements Serializable {
-    public ArrayList<String> allTracks;
-    public ArrayList<String> currentPlayList;
+    private class SerializableTrack implements Serializable {
+        private String path;
+        private Boolean songLiked;
+        public SerializableTrack(String path, Boolean songLiked) {
+            this.path = path;
+            this.songLiked = songLiked;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public Boolean getSongLiked() {
+            return songLiked;
+        }
+    }
+
+    public ArrayList<SerializableTrack> allTracks;
+    public ArrayList<SerializableTrack> currentPlayList;
     public Integer currentTrackID;
     public Double currentVolume;
     public Boolean isShuffling;
     public Boolean isRepeating;
     public PlayerState(Player player) {
-        allTracks = new ArrayList<>(player.getAllTracks().stream().map(Track::getFilePath).toList());
-        currentPlayList = new ArrayList<>(player.getCurrentPlayList().stream().map(Track::getFilePath).toList());
+        allTracks = new ArrayList<>(player.getAllTracks().stream().map(o -> new SerializableTrack(o.getFilePath(), o.getSongLiked())).toList());
+        currentPlayList = new ArrayList<>(player.getCurrentPlayList().stream().map(o -> new SerializableTrack(o.getFilePath(), o.getSongLiked())).toList());
         currentTrackID = player.getCurrentTrackID().get();
         currentVolume = player.getCurrentVolume().get();
         isShuffling = player.getIsShuffling().get();
         isRepeating = player.getIsRepeating().get();
     }
     public void initPlayer(Player player) {
-        player.getAllTracks().setAll(allTracks.stream().map(item -> {
+        Function<SerializableTrack, Track> func = item -> {
             try {
-                return MP3Parser.parse(new File(URI.create(item)));
+                Track track = MP3Parser.parse(new File(URI.create(item.getPath())));
+                track.setSongLiked(item.getSongLiked());
+                return track;
             } catch (InvalidDataException | UnsupportedTagException | IOException e) {
                 throw new RuntimeException(e);
             }
-        }).toList());
-        player.getCurrentPlayList().setAll(currentPlayList.stream().map(item -> {
-            try {
-                return MP3Parser.parse(new File(URI.create(item)));
-            } catch (InvalidDataException | UnsupportedTagException | IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).toList());
+        };
+        player.getAllTracks().setAll(allTracks.stream().map(func).toList());
+        player.getCurrentPlayList().setAll(currentPlayList.stream().map(func).toList());
         player.setCurrentTrackID(currentTrackID);
         player.setIsShuffling(isShuffling);
         player.setIsRepeating(isRepeating);
